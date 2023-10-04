9ea5a48f46a2d824793a19819ba487cbd0a70688
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-10-04 00:18:13 -0600

    creating-users-model

---
+++ src/users/users.model.js
@@ -0,0 +1,34 @@
+import { DataTypes } from "sequelize"
+import sequelize from "../config/database/database.js"
+
+const User = sequelize.define('users', {
+  id: {
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    field: 'user_id'
+  },
+  name: {
+    type: DataTypes.STRING(200),
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.ENUM("client", "employee"),
+    allowNull: false
+  },
+  status: {
+    type: DataTypes.ENUM("available", "disable")
+  }
+})
+
+export default User
