ff13afcaccfe2933dd231754552459ed628e3b59
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-10-04 02:05:08 -0600

    adding-routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import { router as userRouter } from './../users/user.route.js'
+import { router as repairsRouter } from './../repairs/repairs.route.js'
 
 /* all routes will be placed in here */
 
 export const router = express.Router()
 
 router.use('/users', userRouter)
+router.use('/repairs', repairsRouter)
