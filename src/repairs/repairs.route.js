8d24fbd002812e95e275cf6d05c99c6d78a6b844
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-10-04 02:18:14 -0600

    adding-routes

--- src/repairs/repairs.route.js
+++ src/repairs/repairs.route.js
@@ -1,3 +1,14 @@
 import express from 'express'
 
 export const router = express.Router()
+
+router
+  .route('/')
+  .get()
+  .post()
+
+router
+  .route('/:id/')
+  .get()
+  .patch()
+  .delete()
